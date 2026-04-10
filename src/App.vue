bdc3d8ce668602894d6080143ec1f212548e808e
JM Mac <johnmarkmancol@gmail.com>
2026-04-10 12:42:47 +0800

    feat: add analytics

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,12 @@
 </template>
 
 <script setup lang="ts">
-import TopBar from './components/TopBar.vue'
-import NavBar from './components/NavBar.vue'
-import HeroSection from './components/HeroSection.vue'
-import ServicesSection from './components/ServicesSection.vue'
-import GallerySection from './components/GallerySection.vue'
-import TestimonialsSection from './components/TestimonialsSection.vue'
-import ContactSection from './components/ContactSection.vue'
-import SiteFooter from './components/SiteFooter.vue'
+import TopBar from "./components/TopBar.vue";
+import NavBar from "./components/NavBar.vue";
+import HeroSection from "./components/HeroSection.vue";
+import ServicesSection from "./components/ServicesSection.vue";
+import GallerySection from "./components/GallerySection.vue";
+import TestimonialsSection from "./components/TestimonialsSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import './style.css'
+
+inject()
 
 createApp(App).use(createPinia()).mount('#app')
